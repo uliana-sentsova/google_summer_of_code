9aea0feaaf67cfc092362728cf561f574d4562f3
uliana-sentsova <uliana.sentsova@gmail.com>
2016-05-22 20:48:09 +0300

    scraper modified

--- scraper.py
+++ scraper.py
@@ -8,47 +8,57 @@ with open("sicilian_verbs.txt", "w") as verbs_file, open("translation.txt", "w")
 
     while next_link:
 
-        previous_link = next_link
+        try:
 
-        response = requests.get(next_link)
-        parsed_body = html.fromstring(response.text)
+            previous_link = next_link
 
-        # Find verbs on the page
-        sicilian = parsed_body.xpath('//ul/li/a/@href')
-        for i in range(0, 200):
-            url = "https://scn.wiktionary.org" + sicilian[i]
-            response = requests.get(url)
-            parsed = html.fromstring(response.text)
-            first_section = parsed.xpath("//ul/li/a/text()")
-            second_section = parsed.xpath("//dl/dd/a/text()")
-            if second_section:
-                for part in second_section:
-                    first_section.append(part)
-            verb = sicilian[i].split("i/")[1].strip()
+            response = requests.get(next_link)
+            parsed_body = html.fromstring(response.text)
 
-            # Replace UTF
-            verb = verb.replace("%C3%AC", "ì")
-            verb = verb.replace("%C3%B2", "ò")
-            verb = verb.replace("%C3%A0", "à")
-            verb = verb.replace("%C3%A8", "è")
-            verb = verb.replace("%C3%B9", "ù")
+            # Find verbs on the page
+            sicilian = parsed_body.xpath('//ul/li/a/@href')
+            for i in range(0, 200):
+                url = "https://scn.wiktionary.org" + sicilian[i]
+                response = requests.get(url)
+                parsed = html.fromstring(response.text)
+                first_section = parsed.xpath("//ul/li/a/text()")
+                second_section = parsed.xpath("//dl/dd/a/text()")
+                if second_section:
+                    for part in second_section:
+                        first_section.append(part)
+                verb = sicilian[i].split("i/")[1].strip()
 
-            verbs_file.write(verb + "\n")
-            translation_file.write(verb + "\t" + ",".join(first_section) + "\n")
-            count += 1
-            if count%50 == 0:
-                print(count, "verbs extracted.")
+                if not verb.endswith("ri") or verb.endswith("si"):
+                    continue
 
-        # Look for the next page:
-        hrefs_list = parsed_body.xpath('//div/a/@href')
-        hrefs_list = list(set(hrefs_list))
-        for link in hrefs_list:
-            if "pagefrom" in link:
-                next_link = "https://scn.wiktionary.org" + link
+                # Replace UTF
+                verb = verb.replace("%C3%AC", "ì")
+                verb = verb.replace("%C3%B2", "ò")
+                verb = verb.replace("%C3%A0", "à")
+                verb = verb.replace("%C3%A8", "è")
+                verb = verb.replace("%C3%B9", "ù")
 
-        print(next_link)
-        print("NEXT PAGE")
+                verbs_file.write(verb + "\n")
+                translation_file.write(verb + "\t" + ",".join(first_section) + "\n")
+                count += 1
+                if count%50 == 0:
+                    print(count, "verbs extracted. Last verb:", verb)
 
-        # If there is no next page, break
-        if next_link == previous_link:
-            break
+            # Look for the next page:
+            hrefs_list = parsed_body.xpath('//div/a/@href')
+            hrefs_list = list(set(hrefs_list))
+            for link in hrefs_list:
+                if "pagefrom" in link:
+                    next_link = "https://scn.wiktionary.org" + link
+
+            print(next_link)
+            print("NEXT PAGE")
+
+            # If there is no next page, break
+            if next_link == previous_link:
+                break
+
+        # It is a terrible exception handler, but still, I need it.
+        except Exception as err:
+            print(err)
+            pass
